38f44b7a945e3f5dfd2436cacfe42efca539640c
Vitor Cogo <vitor_cogo@outlook.com>
2024-10-26 12:14:47 -0300

    implementacao final

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
 
-$.ajax({
-  url: 'https://jsonplaceholder.typicode.com/posts',
-  type: 'GET',
-  success: function(data) {
-    console.log(data);
-    var container = $('#postsContainer');
+const loadPosts = () => {
+  const loading = $('.load-section');
+  const content = $('.main-section');
+  loading.show();
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    type: 'GET',
+    success: (data) => {
+      loading.hide();
+      content.show();
+      content.css('display', 'flex');
 
-    data.forEach(function(post) {
-      var postElement = `
-        <div class="post" id="postId-${post.id}">
-          <div class="title">
-            ${post.title}
+      var container = $('#postsContainer');
+  
+      data.forEach((post) => {
+        var postElement = `
+          <div class="post" id="postId-${post.id}" data-post-id="${post.id}">
+            <div class="title">
+              ${post.title}
+            </div>
+            <div class="resume">
+              ${post.body.replace(/\n/g, '<br>')}
+            </div>
+            <div class="comments-container" id="commentsContainer-${post.id}"></div>
+            <button class="show-comments" data-post-id="${post.id}">Ver comentáros</button>
+            <button class="hide-comments" data-post-id="${post.id}">Esconder comentários</button>
+            <button class="loading-comments" data-post-id="${post.id}">
+              <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" alt="">
+              Carregando
+            </button>
           </div>
-          <div class="resume">
-            ${post.body.replace(/\n/g, '<br>')}
-          </div>
-          <div class="comments-container" id="commentsContainer-${post.id}"></div>
-          <button class="show-comments" data-post-id="${post.id}">Ver comentáros</button>
-          <button class="hide-comments" data-post-id="${post.id}">Esconder comentários</button>
-        </div>
-      `;
+        `;
+  
+        container.append(postElement);
+      });
+    }
+  });
+}
 
-      container.append(postElement);
-    });
-  }
-});
-
-$('#postsContainer').on('click', '.show-comments', function() {
-  const button = $(this);
-  const postId = button.data('post-id');
+const loadComments = (postId) => {
   const commentsContainer = $(`#commentsContainer-${postId}`);
+  const showButton = $(`#postId-${postId} .show-comments`);
   const hideButton = $(`#postId-${postId} .hide-comments`);
+  const loadingButton = $(`#postId-${postId} .loading-comments`);
+
+  showButton.hide();
+  loadingButton.show();
 
   const url = `https://jsonplaceholder.typicode.com/comments?postId=${postId}`;
   fetch(url)
     .then(response => response.json())
     .then(comments => {
-      comments.forEach(function(comment) {
-        var commentElement = `
+      commentsContainer.empty();
+      comments.forEach((comment) => {
+        const commentElement = `
           <div class="comment">
             <img src="https://www.svgrepo.com/show/327465/person-circle.svg" alt="User Icon">
             <div class="msg-container">
@@ -49,18 +65,62 @@ $('#postsContainer').on('click', '.show-comments', function() {
         `;
         commentsContainer.append(commentElement);
       });
-      button.hide();
+
+      loadingButton.hide();
       hideButton.show();
     });
+}
+
+const hideComments = (postId) => {
+  const commentsContainer = $(`#commentsContainer-${postId}`);
+  const showButton = $(`#postId-${postId} .show-comments`);
+  const hideButton = $(`#postId-${postId} .hide-comments`);
+
+  commentsContainer.empty();
+  hideButton.hide();
+  showButton.show();
+}
+
+$('#postsContainer').on('click', '.show-comments', function() {
+  const button = $(this);
+  const postId = button.data('post-id');
+  loadComments(postId);
+});
+
+$('#mainSection').on('click', '.see-all-comments', function() {
+  const button = $(this);
+  const loadingButton = $(`.loading-all-comments`);
+  const hideButton = $(`.hide-all-comments`);
+
+  button.hide();
+  loadingButton.show();
+
+  $('#postsContainer .post').each(function() {
+    const postId = $(this).data('post-id');
+    loadComments(postId);
+  });
+
+  loadingButton.hide();
+  hideButton.show();
+});
+
+$('#mainSection').on('click', '.hide-all-comments', function() {
+  const button = $(this);
+  const showButon = $(`.see-all-comments`);
+
+  $('#postsContainer .post').each(function() {
+    const postId = $(this).data('post-id');
+    hideComments(postId);
+  });
+  
+  button.hide();
+  showButon.show();
 });
 
 $('#postsContainer').on('click', '.hide-comments', function() {
   const button = $(this);
   const postId = button.data('post-id');
-  const commentsContainer = $(`#commentsContainer-${postId}`);
-  const showButton = $(`#postId-${postId} .show-comments`);
-
-  commentsContainer.empty();
-  button.hide();
-  showButton.show();
+  hideComments(postId);
 });
+
+loadPosts();
